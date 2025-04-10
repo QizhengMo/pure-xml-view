dd20679d6883788cbb322606ed896072768b0576
qzmo <qzmo@trip.com>
2025-04-10 20:05:55 +0800

    fix: react binding

--- src/pure-xml-react.tsx
+++ src/pure-xml-react.tsx
@@ -1,32 +1,25 @@
-import React, { useRef, useEffect, forwardRef, ButtonHTMLAttributes } from 'react';
+import React, { forwardRef, useEffect, useRef } from "react";
 import { PureXml } from "./pure-xml-view";
 
 // Ensure the web component is defined
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   // This is to ensure the component is only registered once
-  if (!customElements.get('pure-xml-view')) {
-    customElements.define('pure-xml-view', PureXml);
+  if (!customElements.get("pure-xml-view")) {
+    customElements.define("pure-xml-view", PureXml);
   }
 }
 
-export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> {
-  variant?: 'primary' | 'secondary';
-  onClick?: (event: CustomEvent) => void;
+export interface Props {
+  data: string;
 }
 
-export const PureXmlView = forwardRef<HTMLElement, ButtonProps>(({
-                                                              children,
-                                                              variant = 'primary',
-                                                              disabled,
-                                                              onClick,
-                                                              ...props
-                                                            }, ref) => {
+export const PureXmlView = forwardRef<HTMLElement, Props>(({ data }, ref) => {
   const componentRef = useRef<HTMLElement | null>(null);
 
   // Combine refs
   useEffect(() => {
     if (ref) {
-      if (typeof ref === 'function') {
+      if (typeof ref === "function") {
         ref(componentRef.current);
       } else {
         ref.current = componentRef.current;
@@ -34,14 +27,10 @@ export const PureXmlView = forwardRef<HTMLElement, ButtonProps>(({
     }
   }, [ref]);
 
-
   return (
     // @ts-ignore
-    <pure-xml-view ref={ref}>
-      {children}
-    {/*// @ts-ignore*/}
-    </pure-xml-view>
+    <pure-xml-view ref={ref} data={data} />
   );
 });
 
-PureXmlView.displayName = 'Pure-Xml-View';
+PureXmlView.displayName = "Pure-Xml-View";
